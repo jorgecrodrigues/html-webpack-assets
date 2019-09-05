424e65c0024738a5af5b396e0a708e44034ebede
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-09-05 10:59:48 -0400

    Adicionado código para gerenciar os assets e o arquivo de pacotes do npm

---
+++ index.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+/**
+ * Inject chunks inside tag head or body
+ *
+ * @param headChunks
+ * @param bodyChunks
+ * @return {{b: *, h: *}}
+ */
+function getHeadAndBodyChunks(headChunks, bodyChunks) {
+    const onHead = [];
+    const onBody = [];
+
+    bodyChunks.forEach(chunk => {
+        if (chunk.attributes.src && chunk.attributes.src.includes("head")) {
+            onHead.push(chunk);
+        } else {
+            onBody.push(chunk);
+        }
+    });
+
+    headChunks.forEach(chunk => {
+        if (chunk.attributes.src && chunk.attributes.src.includes("body")) {
+            onBody.push(chunk);
+        } else {
+            onHead.push(chunk);
+        }
+    });
+
+    return {onHead, onBody};
+}
+
+class HtmlWebpackAssetsPlugin {
+    apply(compiler) {
+        // HtmlWebpackPlugin version 4.0.0-beta.5
+        if (HtmlWebpackPlugin.getHooks) {
+            compiler.hooks.compilation.tap('HtmlWebpackAssetsPlugin', (compilation) => {
+                HtmlWebpackPlugin.getHooks(compilation).alterAssetTagGroups.tapAsync(
+                    'HtmlWebpackAssetsPlugin', (data, callback) => {
+                        const ch = getHeadAndBodyChunks(data.headTags, data.bodyTags);
+                        data.headTags = ch.onHead;
+                        data.bodyTags = ch.onBody;
+                        callback(null, data)
+                    }
+                )
+            });
+        } else {
+            // HtmlWebpackPlugin version 3.2.0
+            compiler.plugin("compilation", compilation => {
+                compilation.plugin("html-webpack-plugin-alter-asset-tags", data => {
+                    const ch = getHeadAndBodyChunks(data.head, data.body);
+                    data.head = ch.onHead;
+                    data.body = ch.onBody;
+                });
+            });
+        }
+    }
+}
+
+module.exports = HtmlWebpackAssetsPlugin;
